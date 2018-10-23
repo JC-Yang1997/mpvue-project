ef3a00be60b3db95f6b6543499490f3f7e2f7a07
yangjinchun <2537346378@qq.com>
2018-10-23 09:39:31 +0800

    init:清理mpvue脚手架目录（基本项目构建完成）

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>{{message}}</div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            message :"hello yangjinchun"
+        };
+    },
+    components:{
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    beforecreate(){
+
+    },
+    created(){
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
