a1dbf2576bb703daf84b3cb50702482ee07b1815
yangjinchun <2537346378@qq.com>
2018-10-23 17:41:56 +0800

    fix:修改轮播图bug，简化代码等

--- src/components/test-cp1/index.vue
+++ src/components/test-cp1/index.vue
@@ -1,18 +1,42 @@
 <template>
     <div>
-        <div>{{content}}</div>
+    <swiper indicator-dots autoplay circular interval="3000" duration="1000">
+        <block>
+            <swiper-item :class="swiper-item" v-for="(item,index) in imgUrls" :key="index">
+                <image :src="item" class="slide-image"/>
+            </swiper-item>
+        </block>
+    </swiper>
     </div>
 </template>
 <script>
 export default {
     data(){
         return {
-            content:"component1"
+             imgUrls: [
+                "http://dummyimage.com/300x200/79f2a2&text=Michael Clark",
+                "http://dummyimage.com/300x200/f27f79&text=Susan Lee",
+                "http://dummyimage.com/300x200/7995f2&text=William Garcia",
+                "http://dummyimage.com/300x200/b9f279&text=Jessica Rodriguez",
+                "http://dummyimage.com/300x200/f279dc&text=Brenda Hall"
+            ],
         };
     }
 }
 </script>
 <style scoped>
-
+swiper{
+    height: 200px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 10px;
+}
+.slide-image{
+    height: 100%;
+    width: 100%;
+}
 </style>
 
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -8,20 +8,15 @@
                 <div 
                     class="title_router"
                     @click="handleTitleItem(index)">
-                    {{item.title}}
+                    {{item}}
                 </div>
+                <!-- 路由页面跳转样式用navigator -->
                <!-- <navigator 
                     :url="item.router"
                     class="title_router">{{item.title}}</navigator> -->
            </div>
        </div>
-       <swiper indicator-dots autoplay interval="3000" duration="1000">
-        <block>
-            <swiper-item :class="swiper-item" v-for="(item,index) in imgUrls" :key="index">
-                <image :src="item" class="slide-image"/>
-            </swiper-item>
-        </block>
-        </swiper>
+        <!-- 组件切换样式用v-if判断 -->
         <div>
             <div v-if="componentChooseId == 0"><test-cp-one /></div>
             <div v-if="componentChooseId == 1"><test-cp-two /></div>
@@ -31,37 +26,15 @@
     </div>
 </template>
 <script>
-import testCpOne from "../../components/test-cp1/index.vue";
-import testCpTwo from "../../components/test-cp2/index.vue";
-import testCpThree from "../../components/test-cp3/index.vue";
-import testCpFour from "../../components/test-cp4/index.vue";
+import testCpOne from "@/components/test-cp1/index.vue";
+import testCpTwo from "@/components/test-cp2/index.vue";
+import testCpThree from "@/components/test-cp3/index.vue";
+import testCpFour from "@/components/test-cp4/index.vue";
 export default {
     data(){
         return {
             titleList:[
-                {
-                    title:"页面1",
-                    router:"/pages/test-page1/main"
-                },
-                {
-                    title:"页面2",
-                    router:"/pages/test-page2/main"
-                },
-                {
-                    title:"页面3",
-                    router:"/pages/test-page3/main"
-                },
-                {
-                    title:"页面4",
-                    router:"/pages/test-page4/main"
-                },
-            ],
-            imgUrls: [
-                "http://dummyimage.com/300x200/79f2a2&text=Michael Clark",
-                "http://dummyimage.com/300x200/f27f79&text=Susan Lee",
-                "http://dummyimage.com/300x200/7995f2&text=William Garcia",
-                "http://dummyimage.com/300x200/b9f279&text=Jessica Rodriguez",
-                "http://dummyimage.com/300x200/f279dc&text=Brenda Hall"
+                "页面1","页面2","页面3","页面4"
             ],
             componentChooseId:0
         };
@@ -78,7 +51,7 @@ export default {
        }
     },
     computed:{
-
+        
     },
     watch:{
 
@@ -113,21 +86,8 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: yellow;
-    color: white;
+    background-color: #000;
+    color: #fff;
     font-weight: bolder;
-}
-swiper{
-    height: 200px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-sizing: border-box;
-    padding: 10px;
-}
-.slide-image{
-    height: 100%;
-    width: 100%;
 }
 </style>
